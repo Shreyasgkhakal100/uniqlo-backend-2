a7b8703f03f7567efbeb64665cbe90bd5ccb4781
Himanshu <hgauba4@gmail.com>
2022-02-24 15:34:31 +0530

    djdk

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,4 +12,28 @@ router.get("",async(req,res)=>{
         res.status(500).send(err.message)
     }
 })
+router.get("/:id",async(req,res)=>{
+    try{
+        let users=await User.find(req.params.id).lean().exec();
+        res.send(users)
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+})
+router.patch("/:id",async(req,res)=>{
+    try{
+        let users=await User.findByIdAndUpdate(req.params.id,req.body,{new:true}).lean().exec();
+        res.send(users)
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+})
+router.delete("/:id",async(req,res)=>{
+    try{
+        let users=await User.findByIdAndDelete(req.params.id,req.body,{new:true}).lean().exec();
+        res.send(users)
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+})
 module.exports = router;
